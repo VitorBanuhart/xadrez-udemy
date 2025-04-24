da6ad4cba17eef72b91e44bc9e7883d716c0be37
Vitor Banuhart <vitorbanuhart123@gmail.com>
2025-04-24 15:10:59 -0300

    printing possible moves

--- src/application/chess/pieces/Rook.java
+++ src/application/chess/pieces/Rook.java
@@ -23,7 +23,7 @@ public class Rook extends ChessPiece {
 
         Position p = new Position(0, 0);
         
-                //above
+        //above
         p.setValues(position.getRow() - 1, position.getColumn());
         while (getBoard().positionExists(p) && !getBoard().thereUsAPiece(p)) {
             mat[p.getRow()][p.getColumn()] = true;
