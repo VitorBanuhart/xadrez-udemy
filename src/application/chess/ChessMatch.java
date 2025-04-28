fb2b1f75537ee8f2f667c2e048027342d9a24cc0
Vitor Banuhart <vitorbanuhart123@gmail.com>
2025-04-28 16:53:39 -0300

    Queen

--- src/application/chess/ChessMatch.java
+++ src/application/chess/ChessMatch.java
@@ -2,6 +2,7 @@ package application.chess;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Queue;
 import java.util.stream.Collectors;
 
 import application.boardgame.Board;
@@ -11,6 +12,7 @@ import application.chess.pieces.Bishop;
 import application.chess.pieces.King;
 import application.chess.pieces.Knight;
 import application.chess.pieces.Pawm;
+import application.chess.pieces.Queen;
 import application.chess.pieces.Rook;
 
 public class ChessMatch {
@@ -200,6 +202,7 @@ public class ChessMatch {
         placeNewPiece('b', 8, new Knight(board, Color.BLACK));
         placeNewPiece('c', 8, new Bishop(board, Color.BLACK));
         placeNewPiece('d', 8, new King(board, Color.BLACK));
+        placeNewPiece('e', 8, new Queen(board, Color.BLACK));
         placeNewPiece('f', 8, new Bishop(board, Color.BLACK));
         placeNewPiece('g', 8, new Knight(board, Color.BLACK));
         placeNewPiece('a', 8, new Rook(board, Color.BLACK));
@@ -216,6 +219,7 @@ public class ChessMatch {
         placeNewPiece('b', 1, new Knight(board, Color.WHITE));
         placeNewPiece('f', 1, new Bishop(board, Color.WHITE));
         placeNewPiece('d', 1, new King(board, Color.WHITE));
+        placeNewPiece('e', 1, new Queen(board, Color.WHITE));
         placeNewPiece('c', 1, new Bishop(board, Color.WHITE));
         placeNewPiece('g', 1, new Knight(board, Color.WHITE));
         placeNewPiece('a', 1, new Rook(board, Color.WHITE));
